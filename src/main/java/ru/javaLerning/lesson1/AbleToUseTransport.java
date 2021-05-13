c5cdee95a63b92e948e04a443bb8df57244bcccd
Alexey Rassolov <alexey.rassolov@gmail.com>
2021-05-13 08:38:35 +0300

    more close to exercise statement
    to private

--- src/main/java/ru/javaLerning/lesson1/AbleToUseTransport.java
+++ src/main/java/ru/javaLerning/lesson1/AbleToUseTransport.java
@@ -1,5 +1,29 @@
 package ru.javaLerning.lesson1;
 
 public abstract class AbleToUseTransport {
-    protected Transport currentTransport;
+    private Transport currentTransport;
+
+    public void ride(Transport transport) {
+        if (currentTransport != null) {
+            if (currentTransport == transport) {
+                System.out.println("Человек уже двигается конкретно на этом транспорте типа " + currentTransport);
+            } else {
+                System.out.println("Человек не может менять транспорт во время движения. Движение на транспорте типа " + currentTransport + " продолжается");
+            }
+        } else {
+            currentTransport = transport;
+
+            System.out.println("Человек начал передвигаться на транспорте типа " + currentTransport);
+        }
+    }
+
+    public void stop() {
+        if (currentTransport == null) {
+            System.out.println("Человек остался стоять неподвижным");
+        } else {
+            System.out.println("Человек закончил передвигаться на транспорте типа " + currentTransport);
+
+            currentTransport = null;
+        }
+    }
 }
--- src/main/java/ru/javaLerning/lesson1/Person.java
+++ src/main/java/ru/javaLerning/lesson1/Person.java
@@ -6,28 +6,4 @@ public class Person extends AbleToUseTransport {
     }
 
     private final String name;
-
-    public void ride(Transport transport) {
-        if (currentTransport != null) {
-            if (currentTransport == transport) {
-                System.out.println(name + " уже двигается конкретно на этом транспорте типа " + currentTransport);
-            } else {
-                System.out.println(name + " не может менять транспорт во время движения. Движение на транспорте типа " + currentTransport + " продолжается");
-            }
-        } else {
-            currentTransport = transport;
-
-            System.out.println(name + " начал передвигаться на транспорте типа " + currentTransport);
-        }
-    }
-
-    public void stop() {
-        if (currentTransport == null) {
-            System.out.println(name + " остался стоять неподвижным");
-        } else {
-            System.out.println(name + " закончил передвигаться на транспорте типа " + currentTransport);
-
-            currentTransport = null;
-        }
-    }
 }
